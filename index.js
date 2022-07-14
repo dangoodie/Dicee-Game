b57d6993a1953de762444c4c7a73d0f5563f76c3
dangoodie <70499319+dangoodie@users.noreply.github.com>
2022-07-14 14:39:15 +1000

    Added button to play the game

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-var randomNum1 = rng(1, 6);
-var randomNum2 = rng(1, 6);
+document.querySelector("button").addEventListener("click", playDiceGame);
 
-setDiceImg(randomNum1, ".img1");
-setDiceImg(randomNum2, ".img2");
+function playDiceGame() {
+  var randomNum1 = rng(1, 6);
+  var randomNum2 = rng(1, 6);
 
-displayWinner(randomNum1,randomNum2);
+  setDiceImg(randomNum1, ".img1");
+  setDiceImg(randomNum2, ".img2");
 
-function rng(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+  displayWinner(randomNum1, randomNum2);
 
-function setDiceImg(num, imgclass) {
-    document.querySelector(imgclass).setAttribute("src", "images/dice" + num + ".png");
-}
+  function rng(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
-function displayWinner (num1, num2) {
-    if (num1 == num2) {changeH1Text("Draw!");}
-    if (num1 > num2) {changeH1Text("🚩Player 1 Wins!");}
-    if (num1 < num2) {changeH1Text("Player 2 Wins! 🚩");}
-}
+  function setDiceImg(num, imgclass) {
+    document
+      .querySelector(imgclass)
+      .setAttribute("src", "images/dice" + num + ".png");
+  }
 
-function changeH1Text (string) {
+  function displayWinner(num1, num2) {
+    if (num1 == num2) {
+      changeH1Text("Draw!");
+    }
+    if (num1 > num2) {
+      changeH1Text("🚩Player 1 Wins!");
+    }
+    if (num1 < num2) {
+      changeH1Text("Player 2 Wins! 🚩");
+    }
+  }
+
+  function changeH1Text(string) {
     document.querySelector("h1").textContent = string;
+  }
 }
